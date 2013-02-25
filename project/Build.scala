e652360434b2ae59997bfa6d7d3c9d690c4d0b8f
iseki <iseki.m.aa@gmail.com>
2013-02-26 00:14:08 +0900

    specsのテスト削除

--- project/Build.scala
+++ project/Build.scala
@@ -4,17 +4,18 @@ import PlayProject._
 
 object ApplicationBuild extends Build {
 
-    val appName         = "test"
+    val appName         = "platTestWithJUnit"
     val appVersion      = "1.0-SNAPSHOT"
 
     lazy val s = Defaults.defaultSettings ++ Seq(ScctPlugin.instrumentSettings: _*)
 
     val appDependencies = Seq(
       // Add your project dependencies here,
-      "com.novocode"  % "junit-interface" % "0.10-M2"
+      // "com.novocode"  % "junit-interface" % "0.10-M2"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA, settings=s).settings(
+    // val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
       // Add your own project settings here
       parallelExecution in test := false
     )
