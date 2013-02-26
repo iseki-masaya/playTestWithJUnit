a3bcdea0a8549df06102a5dd95ab3607f272c458
iseki <iseki.m.aa@gmail.com>
2013-02-27 01:44:15 +0900

    変更の修正

--- project/project/plugin.scala
+++ project/project/plugin.scala
@@ -3,6 +3,6 @@ import sbt._
 object Plugins extends Build {
   lazy val plugins = Project("plugins", file("."))
     .dependsOn(
-      uri("https://github.com/iseki-masaya/sbt-simple-junit-xml-reporter-plugin")
+      uri("git://github.com/bseibel/sbt-simple-junit-xml-reporter-plugin.git")
     )
 }
